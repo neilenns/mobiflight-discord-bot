02489b116c94481a6dc5bbfb2dbbdedb9edd0c30
Neil Enns <neile@live.com>
2024-01-15 10:41:08 -0800

    Allow specifying the topic on the slash command

--- src/commands/general/wiki.js
+++ src/commands/general/wiki.js
@@ -28,50 +28,85 @@ menuItems.forEach((item) => {
   );
 });
 
+// Prompts the user to pick a wiki topic from the dropdown.
+// This function will throw an error if anything goes wrong.
+async function promptForTopic(interaction) {
+  const row = new ActionRowBuilder().addComponents(select);
+
+  // Send the menu
+  const menu = await interaction.reply({
+    content: "Select a topic",
+    components: [row],
+    ephemeral: true,
+  });
+
+  // Wait for the menu response
+  const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+  const confirmation = await menu.awaitMessageComponent({
+    filter: collectorFilter,
+    time: 60_000,
+  });
+
+  return confirmation.values[0];
+}
+
+async function replyOrEditReply(interaction, options) {
+  if (interaction.replied) {
+    await interaction.editReply(options);
+  } else {
+    await interaction.reply(options);
+  }
+}
+
 module.exports = {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName("wiki")
-    .setDescription("Links to wiki topics"),
+    .setDescription("Links to wiki topics")
+    .addStringOption((option) =>
+      option
+        .setName("topic")
+        .setDescription("The name of the wiki topic to send")
+        .setRequired(false)
+    ),
   async execute(interaction) {
-    const row = new ActionRowBuilder().addComponents(select);
-
-    // Send the menu
-    const menu = await interaction.reply({
-      content: "Select a topic",
-      components: [row],
-      ephemeral: true,
-    });
-
-    // Wait for the menu response
-    const collectorFilter = (i) => i.user.id === interaction.user.id;
-
     try {
-      const confirmation = await menu.awaitMessageComponent({
-        filter: collectorFilter,
-        time: 60_000,
-      });
+      // Check and see if a topic was provided on the command.
+      let topic;
+      topic = interaction.options.getString("topic") ?? null;
+
+      if (topic === null) {
+        topic = await promptForTopic(interaction);
+      }
 
       // Find the selected item
-      const selectedItem = menuItems.find(
-        (item) => item.value === confirmation.values[0]
-      );
+      const selectedItem = menuItems.find((item) => item.value === topic);
 
+      if (selectedItem === undefined) {
+        await replyOrEditReply(interaction, {
+          content: `No wiki entry for ${topic} found`,
+          ephemeral: true,
+        });
+        return;
+      }
       const link = hyperlink(selectedItem.description, selectedItem.href);
+
+      await replyOrEditReply(interaction, {
+        content: `Link sent!`,
+        components: [],
+        ephemeral: true,
+      });
 
       await interaction.channel.send({
         content: `Check out this wiki page for information: ${link}`,
       });
-      await interaction.editReply({
-        content: `Link sent!`,
+    } catch (error) {
+      console.log(`Unable to send wiki link: ${error}`);
+      await replyOrEditReply(interaction, {
+        content: `Unable to send wiki link: ${error}`,
         components: [],
-        ephemeral: false,
-      });
-    } catch (e) {
-      console.log(`Error: ${e}`);
-      await interaction.editReply({
-        content: "Confirmation not received within 1 minute, cancelling",
-        components: [],
+        ephemeral: true,
       });
     }
   },
