44b66ec5929a13fbe2952beccc0582c0ae0bc7a8
Neil <neile@live.com>
2024-01-14 17:06:54 -0800

    Add exception handling

--- src/events/threadUpdate.js
+++ src/events/threadUpdate.js
@@ -18,36 +18,40 @@ function wasUnSolved(oldThread, newThread, tagId) {
 
 // Returns the tagId for the tag named tagName in the given channel
 function findTagIdByName(channel, tagName) {
-  return (
-    channel.availableTags.filter((item) => {
-      return item.name == tagName;
-    })?.[0].id ?? 0
-  );
+  const matchingTags = channel.availableTags.filter((item) => {
+    return item.name.toLowerCase() == tagName.toLowerCase();
+  });
+
+  return matchingTags.length > 0 ? matchingTags[0].id : 0;
 }
 
 module.exports = {
   name: Events.ThreadUpdate,
   async execute(oldThread, newThread) {
-    // Get the tagId for "Solved" from the parent channel
-    const tagId = findTagIdByName(
-      newThread.parent,
-      process.env.SOLVED_TAG_NAME
-    );
-
-    if (wasSolved(oldThread, newThread, tagId)) {
-      await newThread.send(
-        `Since this is resolved I'm locking the thread. For additional questions or similar issues please start a new thread in <#${newThread.parentId}>.`
+    try {
+      // Get the tagId for "Solved" from the parent channel
+      const tagId = findTagIdByName(
+        newThread.parent,
+        process.env.SOLVED_TAG_NAME
       );
-      newThread.setLocked(true);
-    } else if (wasUnSolved(oldThread, newThread, tagId)) {
-      newThread.setLocked(false);
 
-      // This will only have a value if the cache contains the last message.
-      // for the channel.
-      const lastMessage = newThread.lastMessage;
-      if (lastMessage?.author.bot) {
-        await newThread.lastMessage.delete();
+      if (wasSolved(oldThread, newThread, tagId)) {
+        await newThread.send(
+          `Since this is resolved I'm locking the thread. For additional questions or similar issues please start a new thread in <#${newThread.parentId}>.`
+        );
+        await newThread.setLocked(true);
+      } else if (wasUnSolved(oldThread, newThread, tagId)) {
+        await newThread.setLocked(false);
+
+        // This will only have a value if the cache contains the last message.
+        // for the channel.
+        const lastMessage = newThread.lastMessage;
+        if (lastMessage?.author.bot) {
+          await newThread.lastMessage.delete();
+        }
       }
+    } catch (error) {
+      console.log(`Error setting thread lock state: ${error.message}`);
     }
   },
 };
